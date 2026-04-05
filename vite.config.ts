575683829a644e309ede3913087de59300c2aaf3
CS23students <153345000+CS23students@users.noreply.github.com>
2026-04-05 18:11:23 +0530

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  base: "/",
+  plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "about.html"),
+        services: resolve(__dirname, "services.html"),
+        blog: resolve(__dirname, "blog.html"),
+        contact: resolve(__dirname, "contact.html"),
+        newsletter: resolve(__dirname, "newsletter.html"),
+      },
+    },
+  },
+});
